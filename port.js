9e3b31ab76d783aa297b3ee9cc039c0fad029fdf
autumn-ragland <raglanda998@gmail.com>
2019-02-15 12:11:35 -0600

    0215 morning

--- port.js
+++ port.js
@@ -1,5 +1,9 @@
 //buttons that show the page name when hovered on
 //these buttons are linked to the other site pages
+$("#homeBtn").hover(homeText,backToIcon);
+function homeText() {
+    $(this).text("home")
+}
 $("#skillsBtn").hover(skillsText,backToIcon);
 function skillsText() {
     $(this).text("skills")
@@ -19,21 +23,6 @@ function reflectionText() {
 //function to change the button back to an icon on hover off
 function backToIcon(){
     $(this).text("");
-    $(this).append("<img src=\'icons/circleicon.png\' alt=\'circleicon\'>");
-}
-//show skills
-//try to get this to work with on hover instead of onclick
-//and find some cooler fonts
-$("#skillsBtnShow").click(showSkills);
-function showSkills() {
-    $(".mySkills").removeAttr("hidden");
-}
-
-//show education
-//try to get this to work with on hover instead of onclick
-//and find some cooler fonts
-$("#eduBtnShow").click(showEdu);
-function showEdu() {
-    $(".myEdu").removeAttr("hidden")
+    $(this).append("<img src=\'icons/orangeC.png\' alt=\'circleicon\'>");
 }
 
